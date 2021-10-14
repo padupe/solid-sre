de0076eacf0690d37378b6b02293c96c20d289ac
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-10-14 16:45:58 -0300

    feat(@Types): Added to the Project.

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import { authRoutes } from './auth.routes';
 import { usersRoutes } from './user.routes';
 
 const router = Router();
 
+router.use(authRoutes);
 router.use('/users', usersRoutes);
 
 export { router };
