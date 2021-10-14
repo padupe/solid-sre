5a144c175e43755b8c744bf9843a124c73596216
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-10-14 16:40:39 -0300

    feat(Prettier): insertion of comments.

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,10 @@
 module.exports = {
+  // Vírgula ou Ponto Vírgula ao final de cada linha
   semi: true,
+  // Aspas Simples
   singleQuote: true,
+  // Número de Espaços do recuo
   tabWidth: 2,
+  // Recuo das linhas com tabulação em vez de espaço
   useTabs: false,
 };
