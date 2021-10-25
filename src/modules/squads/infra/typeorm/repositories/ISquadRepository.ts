37c376fa4bf764a19b5189cb19a7589908f41c88
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-10-25 13:25:52 -0300

    feat(Repositories/Squads): ISquadRepository.ts added to the Project.

---
+++ src/modules/squads/infra/typeorm/repositories/ISquadRepository.ts
@@ -0,0 +1,11 @@
+import { ICreateSquadDTO } from "@modules/squads/dtos/ICreateSquadDTO";
+import { Squad } from "../entities/Squad";
+
+
+interface ISquadRepository {
+    create( { name, email }: ICreateSquadDTO): Promise<Squad>;
+    findByName(name): Promise<Squad>;
+    findByEmail(email): Promise<Squad>;
+};
+
+export { ISquadRepository };
