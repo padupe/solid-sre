eff0675415be501527700af822a362e0ba13903f
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-10-26 08:32:37 -0300

    feat(Shared/Container): Added SquadsRepository.

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,15 @@
 import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { SquadsRepository } from '@modules/squads/infra/typeorm/repositories/SquadsRepository';
+import { ISquadRepository } from '@modules/squads/repositories/ISquadRepository';
 import { container } from 'tsyringe';
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository
+);
+
+container.registerSingleton<ISquadRepository>(
+  'SquadsRepository',
+  SquadsRepository
 );
