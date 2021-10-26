3dbbecad7fdc8819ae52bb46a0b2f2d35ff75265
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-10-26 18:03:12 -0300

    feat(Routes): index.ts. Squad Routes added to the Project.

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { authRoutes } from './auth.routes';
+import { squadsRoutes } from './squad.routes';
 import { usersRoutes } from './user.routes';
 
 const router = Router();
 
 router.use(authRoutes);
 router.use('/users', usersRoutes);
+router.use('/squads', squadsRoutes);
 
 export { router };
