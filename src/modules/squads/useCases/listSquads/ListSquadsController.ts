860e377216536e86ab48fd3598f4c9d14d341e0c
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-10-26 19:30:26 -0300

    feat(ListSquadsController): Added to the Project.

--- src/modules/squads/useCases/listSquads/ListSquadsController.ts
+++ src/modules/squads/useCases/listSquads/ListSquadsController.ts
@@ -1,3 +1,15 @@
-class ListSquadsController {}
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import { ListSquadsUseCase } from './ListSquadsUseCase';
+
+class ListSquadsController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const listSquadsUseCase = container.resolve(ListSquadsUseCase);
+
+    const showAllSquads = await listSquadsUseCase.execute();
+
+    return response.status(200).json(showAllSquads);
+  }
+}
 
 export { ListSquadsController };
