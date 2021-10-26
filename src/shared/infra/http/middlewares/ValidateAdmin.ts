67dab3635686ffe51d2f136c83edfe761b666af9
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-10-26 18:30:05 -0300

    feat(ValidateAdmin): use AppError.

--- src/shared/infra/http/middlewares/ValidateAdmin.ts
+++ src/shared/infra/http/middlewares/ValidateAdmin.ts
@@ -1,4 +1,5 @@
 import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
+import { AppError } from '@shared/errors/AppError';
 import { Request, Response, NextFunction } from 'express';
 
 export async function ValidateAdmin(request: Request, response: Response, next: NextFunction) {
@@ -8,7 +9,7 @@ export async function ValidateAdmin(request: Request, response: Response, next: 
   const validadeUser = await usersRepository.findById(id);
 
   if (!validadeUser.isAdmin) {
-    throw new Error('User is not Admin!');
+    throw new AppError('User is not Admin!', 401);
   }
 
   return next();
