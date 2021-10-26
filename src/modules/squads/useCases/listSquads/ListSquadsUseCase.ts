df2f8136577cab0a0b6668f6243572da6f9cb954
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-10-26 19:25:51 -0300

    feat(ListSquadsUseCase): Added to the Project.

---
+++ src/modules/squads/useCases/listSquads/ListSquadsUseCase.ts
@@ -0,0 +1,20 @@
+import { Squad } from "@modules/squads/infra/typeorm/entities/Squad";
+import { ISquadRepository } from "@modules/squads/repositories/ISquadRepository";
+import { inject, injectable } from "tsyringe";
+
+@injectable()
+class ListSquadsUseCase {
+
+    constructor(
+        @inject('SquadsRepository')
+        private squadsRepository: ISquadRepository
+    ){}
+
+    async execute(): Promise<Squad[]> {
+        const showSquads = await this.squadsRepository.list();
+        return showSquads;
+    }
+
+};
+
+export { ListSquadsUseCase };
