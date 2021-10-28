90923e89a486b8c3701a395d32f2dbd11cdc2873
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-10-27 21:12:28 -0300

    feat(CreateSquadUsersUseCase): Added to the Project.
    feat(ICreateSquadDTO): Paramethers insert.

--- src/modules/squads/dtos/ICreateSquadDTO.ts
+++ src/modules/squads/dtos/ICreateSquadDTO.ts
@@ -1,6 +1,10 @@
+import { User } from "@modules/accounts/infra/typeorm/entities/User";
+
 interface ICreateSquadDTO {
   name: string;
   email: string;
+  users?: User[];
+  id?: string;
 }
 
 export { ICreateSquadDTO };
--- src/modules/squads/useCases/createSquadUsers/CreateSquadUsersUseCase.ts
+++ src/modules/squads/useCases/createSquadUsers/CreateSquadUsersUseCase.ts
@@ -1,6 +1,41 @@
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { ISquadRepository } from "@modules/squads/repositories/ISquadRepository";
+import { AppError } from "@shared/errors/AppError";
+import Squad from "@shared/infra/typeorm/seeds/Squad";
+import { inject, injectable } from "tsyringe";
 
+interface IRequest {
+    squad_id: string;
+    users_id: string[];
+}
 
+@injectable()
 class CreateSquadUsersUseCase {
+
+    constructor(
+        @inject('SquadsRepository')
+        private squadsRepository: ISquadRepository,
+
+        @inject('UsersRepository')
+        private usersRepository: IUsersRepository
+    ){};
+
+    async execute({ squad_id, users_id }: IRequest): Promise<Squad> {
+        const findSquad = await this.squadsRepository.findById(squad_id);
+
+        if(!findSquad) {
+            throw new AppError('Squad does not exists!', 404)
+        }
+
+        const newUsers = await this.usersRepository.findByIds(users_id);
+
+        findSquad.users = newUsers;
+
+        await this.squadsRepository.create(findSquad);
+
+        // @ts-ignore
+        return findSquad;
+    }
 
 };
 
