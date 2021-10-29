10e7428141a4793534d2fea071d98f68f81027e1
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-10-29 18:36:13 -0300

    feat(Routes): Users. List All Users: GET.

--- src/shared/infra/http/routes/user.routes.ts
+++ src/shared/infra/http/routes/user.routes.ts
@@ -1,10 +1,13 @@
 import { CreateUserController } from '@modules/accounts/useCases/createUser/CreateUserController';
+import { ListUsersController } from '@modules/accounts/useCases/listUsers/ListUsersController';
 import { Router } from 'express';
 
 const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
+const listUsersController = new ListUsersController();
 
 usersRoutes.post('/', createUserController.handle);
+usersRoutes.get('/', listUsersController.handle);
 
 export { usersRoutes };
