df29fef3875efc9225460e9e6aad823081ca9782
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-11-01 15:43:57 -0300

    fix(Seed/Squads): created_at.

--- src/shared/infra/typeorm/seeds/Squad.ts
+++ src/shared/infra/typeorm/seeds/Squad.ts
@@ -14,14 +14,14 @@ export default class CreateSquads implements Seeder {
           id: 'c639619f-81f9-4b9c-ba8d-ea4161460ddb',
           name: 'Squad A',
           email: 'squad_a@email.com',
-          created_at: '10-27-2021',
+          created_at: '10-27-2021 14:05:00.548574',
           updated_at: '10-27-2021 14:05:00.548574',
         },
         {
           id: 'b35cc893-98b2-4b13-a888-f4c1c2e36b24',
           name: 'Squad B',
           email: 'squad_b@email.com',
-          created_at: '10-27-2021',
+          created_at: '10-27-2021 14:10:00.548574',
           updated_at: '10-27-2021 14:10:00.548574',
         },
       ])
